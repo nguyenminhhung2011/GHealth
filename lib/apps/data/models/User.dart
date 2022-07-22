60dd0f1f292fbea1cc707dde96f4c511d70bab25
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-07-22 10:14:05 +0700

    1

--- lib/apps/data/models/User.dart
+++ lib/apps/data/models/User.dart
@@ -3,33 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User {
   String username;
   String password;
-  String email;
-  String photoUrl;
-  List followers;
-  List following;
-  String uid;
-  String bio;
 
   User({
     required this.username,
     required this.password,
-    required this.email,
-    required this.photoUrl,
-    required this.followers,
-    required this.following,
-    required this.uid,
-    required this.bio,
   });
 
   Map<String, dynamic> toJson() => {
         'username': username,
         'password': password,
-        'email': email,
-        'photoUrl': photoUrl,
-        'followers': followers,
-        'following': following,
-        'uid': uid,
-        'bio': bio,
       };
 
   static User fromSnap(DocumentSnapshot snap) {
@@ -37,12 +19,6 @@ class User {
     return User(
       username: snapshot['username'],
       password: snapshot['password'],
-      email: snapshot['email'],
-      photoUrl: snapshot['photoUrl'],
-      followers: snapshot['followers'],
-      following: snapshot['following'],
-      uid: snapshot['uid'],
-      bio: snapshot['bio'],
     );
   }
 }
