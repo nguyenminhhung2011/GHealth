b7ff2a1dd4ea96b8c7c5bc903e39a0e756413c7c
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-07-26 00:21:26 +0700

    Add Infor about height, weight, age of profile screen

--- lib/apps/pages/dashboard/profileScreen.dart
+++ lib/apps/pages/dashboard/profileScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:gold_health/apps/global_widgets/GradientText.dart';
 import 'package:gold_health/apps/pages/dashboard/activity_trackerScreen.dart';
 
 import '../../template/misc/colors.dart';
@@ -104,6 +105,16 @@ class ProfileScreen extends StatelessWidget {
                           press: () {},
                         ),
                       ],
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      children: [
+                        InforCard(title: 'Height', data: '180cm'),
+                        Spacer(),
+                        InforCard(title: 'Weight', data: '65Kg'),
+                        Spacer(),
+                        InforCard(title: '22yo', data: 'Age'),
+                      ],
                     )
                   ],
                 ),
@@ -114,6 +125,61 @@ class ProfileScreen extends StatelessWidget {
             flex: (heightDevice / 20 * 13).round(),
             child: Container(),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class InforCard extends StatelessWidget {
+  final String title;
+  final String data;
+  const InforCard({
+    Key? key,
+    required this.title,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: const Offset(2, 3),
+            blurRadius: 20,
+          ),
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: Offset(-2, -3),
+            blurRadius: 20,
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          GradientText(
+            data,
+            gradient: AppColors.colorGradient,
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 17,
+            ),
+          ),
         ],
       ),
     );
