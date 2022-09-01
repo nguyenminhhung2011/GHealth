d6ef89b84a85159510acbba9ddcd1c434743d0b5
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-09-01 19:00:06 +0700

    update data collection

--- lib/apps/controls/dailyPlanController/daily_sleep_controller.dart
+++ lib/apps/controls/dailyPlanController/daily_sleep_controller.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:gold_health/apps/controls/dailyPlanController/tracker_controller.dart';
 import 'package:intl/intl.dart';
@@ -80,6 +81,15 @@ class DailySleepController extends GetxController with TrackerController {
     update();
   }
 
+  initAll1(Sleep data) {
+    choseTime.value = data.bedTime;
+    choseDuration.value =
+        Duration(hours: data.alarm.difference(data.bedTime).inHours);
+    for (var item in data.listDate) {
+      listVariable[weekDayTonInt[item - 1]] = true.obs;
+    }
+  }
+
   Map<String, dynamic> listVariable = {
     'Sun': false.obs,
     'Mon': false.obs,
@@ -106,6 +116,7 @@ class DailySleepController extends GetxController with TrackerController {
     'Fri': 5,
     'Sat': 6,
   };
+  List ntToWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   saveAlarmFirebase() async {
     List<int> dateSelect = [];
     listVariable.forEach((key, value) {
@@ -205,11 +216,12 @@ class DailySleepController extends GetxController with TrackerController {
                         barrierColor: Colors.black54,
                         context: context,
                         builder: (context) {
+                          // initAll1(element);
                           return Dialog(
                             backgroundColor: Colors.transparent,
                             child: Container(
                               width: double.infinity,
-                              height: Get.height / 2,
+                              height: 660,
                               padding: const EdgeInsets.all(10),
                               decoration: BoxDecoration(
                                 color: AppColors.mainColor,
@@ -219,13 +231,209 @@ class DailySleepController extends GetxController with TrackerController {
                                 children: [
                                   const Align(
                                     alignment: Alignment.center,
-                                    child: Text('Edit Time Sleep Schedule',
-                                        style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
-                                        )),
+                                    child: Text(
+                                      'Edit Time Sleep Schedule',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
                                   ),
                                   const SizedBox(height: 20),
+                                  Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: AppColors.primaryColor1
+                                          .withOpacity(0.2),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            SvgPicture.asset(
+                                                'assets/icons/Icon-Bed_alarm.svg'),
+                                            const Text(
+                                              'BedTime',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.grey),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          width: double.infinity,
+                                          height: 130,
+                                          child: CupertinoDatePicker(
+                                            onDateTimeChanged: (value) {
+                                              choseTime.value = value;
+                                            },
+                                            mode: CupertinoDatePickerMode.time,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: AppColors.primaryColor1
+                                          .withOpacity(0.2),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            SvgPicture.asset(
+                                                'assets/icons/Icon-Time.svg'),
+                                            const Text(
+                                              'Hours of sleep',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.grey),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          width: double.infinity,
+                                          height: 130,
+                                          child: CupertinoTimerPicker(
+                                            mode: CupertinoTimerPickerMode.hm,
+                                            onTimerDurationChanged: (value) {
+                                              choseDuration.value = value;
+                                            },
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Container(
+                                    width: double.infinity,
+                                    padding: const EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: AppColors.primaryColor1
+                                          .withOpacity(0.2),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            SvgPicture.asset(
+                                                'assets/icons/Icon-Repeat.svg'),
+                                            const Text(
+                                              'Repeat',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  color: Colors.grey),
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          width: double.infinity,
+                                          height: 80,
+                                          child: SingleChildScrollView(
+                                            scrollDirection: Axis.horizontal,
+                                            child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                children: [
+                                                  'Sun',
+                                                  'Mon',
+                                                  'Tue',
+                                                  'Wed',
+                                                  'Thu',
+                                                  'Fri',
+                                                  'Sat',
+                                                ]
+                                                    .map((e) => InkWell(
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(
+                                                                      100),
+                                                          onTap: () {
+                                                            (listVariable[e]
+                                                                        as RxBool)
+                                                                    .value =
+                                                                !(listVariable[
+                                                                            e]
+                                                                        as RxBool)
+                                                                    .value;
+                                                          },
+                                                          child: Stack(
+                                                            alignment: Alignment
+                                                                .center,
+                                                            children: [
+                                                              Obx(
+                                                                () =>
+                                                                    AnimatedContainer(
+                                                                  curve: Curves
+                                                                      .fastLinearToSlowEaseIn,
+                                                                  duration: const Duration(
+                                                                      milliseconds:
+                                                                          500),
+                                                                  alignment:
+                                                                      Alignment
+                                                                          .center,
+                                                                  height: (listVariable[e]
+                                                                              as RxBool)
+                                                                          .value
+                                                                      ? 45
+                                                                      : 0,
+                                                                  width: (listVariable[e]
+                                                                              as RxBool)
+                                                                          .value
+                                                                      ? 45
+                                                                      : 0,
+                                                                  decoration:
+                                                                      BoxDecoration(
+                                                                    shape: BoxShape
+                                                                        .circle,
+                                                                    border: Border.all(
+                                                                        color: AppColors
+                                                                            .primaryColor1,
+                                                                        width:
+                                                                            2),
+                                                                  ),
+                                                                ),
+                                                              ),
+                                                              Container(
+                                                                padding:
+                                                                    const EdgeInsets
+                                                                        .all(5),
+                                                                height: 45,
+                                                                width: 45,
+                                                                alignment:
+                                                                    Alignment
+                                                                        .center,
+                                                                child: Text(
+                                                                  e,
+                                                                  style: const TextStyle(
+                                                                      fontSize:
+                                                                          13,
+                                                                      color: Colors
+                                                                          .blue,
+                                                                      fontWeight:
+                                                                          FontWeight
+                                                                              .w500),
+                                                                ),
+                                                              ),
+                                                            ],
+                                                          ),
+                                                        ))
+                                                    .toList()),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const Spacer(),
                                   Container(
                                     height: 40,
                                     width: double.infinity,
@@ -234,8 +442,42 @@ class DailySleepController extends GetxController with TrackerController {
                                       color: AppColors.primaryColor1,
                                     ),
                                     child: ElevatedButton(
-                                      onPressed: () {
-                                        Get.back();
+                                      onPressed: () async {
+                                        final r = await Get.dialog(YesNoDialog(
+                                            press: () {
+                                              List<int> dateSelect = [];
+                                              listVariable
+                                                  .forEach((key, value) {
+                                                if ((value as RxBool).value) {
+                                                  dateSelect
+                                                      .add(weekDayTonInt[key]);
+                                                }
+                                              });
+                                              updateDataCollection({
+                                                'id': element.id,
+                                                'bedTime': choseTime.value,
+                                                'alarm': (choseTime.value)
+                                                    .add((choseDuration.value)),
+                                                'isTurnOn': element.isTurnOn,
+                                                'isTurnOn1': element.isTurnOn1,
+                                                'listDate':
+                                                    dateSelect.isNotEmpty
+                                                        ? dateSelect
+                                                        : [1, 2, 3, 4, 5, 6, 7],
+                                              });
+                                              initAll();
+                                              Get.back(result: true);
+                                            },
+                                            question:
+                                                'Do you want Update this schedule ?',
+                                            title1:
+                                                'This sleep time will be updated from your schedule',
+                                            title2: ''));
+                                        print(r);
+                                        if (r == true) {
+                                          Get.back();
+                                        }
+                                        // Get.back();
                                       },
                                       style: ElevatedButton.styleFrom(
                                           shape: RoundedRectangleBorder(
