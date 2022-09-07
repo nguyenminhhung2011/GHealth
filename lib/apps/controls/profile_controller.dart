ef11f872486d4ac6bef24291f36ce47b2927cdb2
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2022-09-07 12:20:13 +0700

    load height to collection

--- lib/apps/controls/profile_controller.dart
+++ lib/apps/controls/profile_controller.dart
@@ -7,6 +7,8 @@ import '../../services/auth_service.dart';
 class ProfileC extends GetxController {
   final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
   Map<String, dynamic> get user => _user.value;
+  final Rx<List<Map<String, dynamic>>> listWeightCollection =
+      Rx<List<Map<String, dynamic>>>([]);
   final Rx<String> _uid = "".obs;
 
   // AgeDuration get getAge {
@@ -24,6 +26,7 @@ class ProfileC extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    getAllWeightCollection();
     updateUser(AuthService.instance.currentUser!.uid);
     //ignore: avoid_print
     print(_user.value['uid']);
@@ -41,16 +44,51 @@ class ProfileC extends GetxController {
     getDataUser(_uid.value);
   }
 
-  addWeightToCollection(int newValue) async {
-    DateTime now = DateTime.now();
-    await firestore
+  getAllWeightCollection() async {
+    listWeightCollection.bindStream(firestore
         .collection('users')
         .doc(AuthService.instance.currentUser!.uid)
         .collection('weight')
-        .add({
-      'data': newValue,
-      'date': now,
-    });
+        .snapshots()
+        .map((event) {
+      List<Map<String, dynamic>> result = [];
+      DateTime now = DateTime.now();
+      for (var item in event.docs) {
+        DateTime date = DateTime.fromMicrosecondsSinceEpoch(
+            item.data()['date'].seconds * 1000);
+        if (date.year == now.year &&
+            date.month == now.month &&
+            date.day == now.day) {
+          result.add({
+            'id': item.id,
+            'data': item.data()['data'],
+            'date': date,
+          });
+          break;
+        }
+      }
+      return result;
+    }));
+  }
+
+  addWeightToCollection(int newValue) async {
+    if (listWeightCollection.value.isNotEmpty) {
+      await firestore
+          .collection('users')
+          .doc(AuthService.instance.currentUser!.uid)
+          .collection('weight')
+          .doc(listWeightCollection.value[0]['id'])
+          .update({'data': newValue});
+    } else {
+      await firestore
+          .collection('users')
+          .doc(AuthService.instance.currentUser!.uid)
+          .collection('weight')
+          .add({
+        'data': newValue,
+        'date': DateTime.now(),
+      });
+    }
   }
 
   updateWeight(int newValue) async {
